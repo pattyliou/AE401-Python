87c60dda2f01cc36081b3ce82f8774bc410ff0c6
pattyliou <78011886+pattyliou@users.noreply.github.com>
2021-01-26 14:45:13 +0800

    test
    try

---
+++ test.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 10:13:21 2020
+
+@author: lo
+"""
+
+print(8)
+print(6.3)
+print("hello")
+
+print(type(6))
+print(type(5.08))
+print(type("hello"))
+
+print('hello2')
+
+print('world')
+
+a = 6
+b = 8
+print(a+b)
+print('68')
+print(a*10+b)
+
+c = "hello"
+d = 'world'
+print(c+d)
+
+
+i=1
+print(i)
+i=i+1
+print(i)
+i=i+2
+print(i)
+i=i+3
+print(i)
+
+i=0
+while i<3:
+    print("patty")
+    i=i+1
+    
+    
+i=0
+while i<6 :
+    print("*"*i)
+    i=i+1
+    
+
+i=0
+while i<6 :
+    print("@"*(6-i)+"*"*i)
+    i=i+1
+    
+    
+i=0
+while i<6 :
+    print(" "*(6-i)+"* "*i)
+    i=i+1
+    
+    
+    
+    
+    
+    
+    
